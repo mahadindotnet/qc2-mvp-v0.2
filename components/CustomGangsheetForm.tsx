2b069e39ea456fd253b99180372eee854339bb68
mahadindotnet <mahadin@softloon.com>
2025-10-05 02:55:07 +0600

    Remove unused Badge import from CustomGangsheetForm

--- components/CustomGangsheetForm.tsx
+++ components/CustomGangsheetForm.tsx
@@ -10,7 +10,6 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Badge } from '@/components/ui/badge'
 
 interface GangsheetElement {
   id: string
